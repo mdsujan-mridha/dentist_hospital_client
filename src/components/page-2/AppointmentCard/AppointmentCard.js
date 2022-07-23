4f072645ff47a68de7ea60acf10f6bdfb547186f
mdsujan-mridha <mdsujan134523@gmail.com>
2022-07-23 18:24:28 +0600

    conditional slots create

--- src/components/page-2/AppointmentCard/AppointmentCard.js
+++ src/components/page-2/AppointmentCard/AppointmentCard.js
@@ -8,9 +8,18 @@ const AppointmentCard = ({timeSlot}) => {
             <div class="card w-96 text-primary-content shadow-2xl">
                 <div class="card-body">
                     <h2 class="text-center mt-5 font-bold text-3xl text-secondary"> {name} </h2>
-                    <p className='text-center font-bold'> {slots.length} spaces available </p>
+                    <p className='text-center'>
+                        {
+                            slots.length >0
+                            ?
+                            <span>{slots[0]} </span>
+                            :
+                            <span className='text-red-500'> No slot available </span>
+                        }
+                    </p>
+                    <p className='text-center font-bold'> {slots.length} {slots.length > 1 ? "spaces" : "space"}  available </p>
                     <div class="card-actions mt-5">
-                        <button className='h-11 text-white text-center font-bold w-72 border rounded-lg bg-secondary '>Book Now</button>
+                        <button disabled={slots.length === 0} className='h-11 text-white text-center font-bold w-72 border rounded-lg bg-secondary'>Book Now</button>
                     </div>
                 </div>
             </div>
